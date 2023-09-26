7b98ff63a31db74fac07024b2bce56a4915de581
Jerry Gerardo Gonzalez Zambrano <jergonza@c3r12s3.42madrid.com>
2023-09-26 14:48:52 +0200

    libft con correciones en makefile

---
+++ ft_calloc.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jergonza <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 17:23:54 by jergonza          #+#    #+#             */
+/*   Updated: 2023/09/13 17:23:56 by jergonza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*temp;
+
+	if (count == SIZE_MAX || size == SIZE_MAX)
+		return (NULL);
+	temp = malloc((count * size));
+	if (temp == NULL)
+		return (NULL);
+	ft_bzero(temp, (count * size));
+	return (temp);
+}
---
+++ ft_putnbr_fd.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jergonza <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/21 11:26:22 by jergonza          #+#    #+#             */
+/*   Updated: 2023/09/21 11:26:24 by jergonza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	num;
+
+	num = (long)n;
+	if (num < 0)
+	{
+		ft_putchar_fd('-', fd);
+		num = -num;
+	}
+	if (num > 9)
+	{
+		ft_putnbr_fd((num / 10), fd);
+		ft_putnbr_fd((num % 10), fd);
+	}
+	else
+		ft_putchar_fd((char)(num + 48), fd);
+}
